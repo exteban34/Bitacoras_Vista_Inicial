7dd39491e6f9b5905fd4c046755c0afb75f23343
Esteban Alvarez <exteban34@gmail.com>
2015-04-30 14:25:22 -0500

    Detalle y Manifest

--- src/com/edu/udea/drai/bitacoras_vista_inicial/Detalle_tarea.java
+++ src/com/edu/udea/drai/bitacoras_vista_inicial/Detalle_tarea.java
@@ -6,8 +6,7 @@ import com.edu.udea.drai.bitacoras_vista_inicial.model.Tarea;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
